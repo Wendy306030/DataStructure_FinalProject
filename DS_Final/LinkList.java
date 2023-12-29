109c22d9b72a84142544a7d399efd98bb8991eb3
Kuoyutzu111 <118511754+Kuoyutzu111@users.noreply.github.com>
2023-12-29 23:59:11 +0800

    Update LinkList.java

--- DS_Final/LinkList.java
+++ DS_Final/LinkList.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class LinkList {
 	public ArrayList<Link> linkList;
@@ -15,29 +16,33 @@ public class LinkList {
 		linkList.add(link);
 	}
 	
+	
+	
 	public void printresult() throws IOException {
 		
 		for(Link li:linkList) {
-			System.out.println(li.getTitle());
-			System.out.println(li.getUrl());
 			sum(li);
-			System.out.println(li.score);
 		}
+		linkList.sort(Comparator.comparingInt(Link::getScore).reversed());
 		
-
+		System.out.println("Sorted by Score:");
+		for(Link li : linkList) {
+			System.out.println(li.getTitle()+"\n"+li.getUrl()+"\n"+"   ～   "+li.getScore());
+		}
 	}
 
 	//計算link裡的分數
 	public void sum(Link li) throws IOException {
 		
 		WordCounter wordCount =new WordCounter(li.getUrl());
+		int totalScore = 0;
 		
 		for(int i=0;i<kLst.lst.size();i++) {
 			int wordNum=wordCount.countKeyword(kLst.lst.get(i).getName());
 			li.setScore(wordNum*kLst.lst.get(i).getWeight());
 		}
-
-	public void getLinkList(){
+		li.setScore(totalScore);
+	
 		
 	}
 
